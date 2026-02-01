66f9bc168f9b18c3c26e415ab5f2fef53c2c7443
ratankumar1234 <1ratantg@gmail.com>
2026-02-01 22:43:17 +0530

    Add Mempool class for transaction management
    Implemented Mempool class to manage transactions and spent UTXOs.

--- src/mempool.cpp
+++ src/mempool.cpp
@@ -1,1 +1,30 @@
+#ifndef MEMPOOL_CPP
+#define MEMPOOL_CPP
+#include "validator.cpp"
+#include <vector>
+using namespace std;
 
+class Mempool {
+public:
+    vector<Transaction> transactions;
+    set<pair<string, int>> spent_utxos;
+    
+    bool add_transaction(Transaction tx, UTXOManager& utxo_mgr) {
+        string error;
+        if (Validator::validate(tx, utxo_mgr, spent_utxos, error)) {
+            transactions.push_back(tx);
+            for (auto& in : tx.inputs) {
+                spent_utxos.insert({in.prev_tx, in.index});
+            }
+            return true;
+        }
+        cout << "Rejected : " << error << endl;
+        return false;
+    }
+    void clear_mempool() {
+        transactions.clear();
+        spent_utxos.clear();
+    }
+};
+
+#endif
